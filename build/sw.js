0eb761765da3ac482dc52420d86d914aa367e8c9
hidayatkhan <hidayat.rehman64@gmail.com>
2022-06-06 15:28:14 +0500

    fixed offline and service worker registration failed issues

---
+++ build/sw.js
@@ -0,0 +1,33 @@
+console.log("service worker from public folder")
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                "/static/js/bundle.js",
+                "index.html",
+                "/login",
+                "/dashboard",
+                "/dashboard/help",
+                "/dashboard/contact",
+                "/dashboard/setting",
+                "/"
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch", (event) => {
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+            })
+        )
+    }
+
+
+})
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorkerRegistration';
+// import * as swDev from './swDev';
+import swDev from "./swDev"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,5 +16,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-serviceWorker.register();
+// reportWebVitals();
+// serviceWorker.register();
+swDev();
